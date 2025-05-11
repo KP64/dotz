ba3b9c886942aae72becae9a2ebd61c05492c931
KP64 <karamalsadeh@hotmail.com>
2025-05-11 20:15:56 +0200

    refactor: apply clippy lints

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+//! A simple feature rich Colorscript.
+
 use clap::{Parser, ValueEnum};
 use crossterm::{
     cursor,
     event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
     execute, queue,
     style::{Color, Print, SetForegroundColor},
-    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen, size},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use itertools::iproduct;
 use std::{
     fmt,
     io::{self, Write},
     time::Duration,
 };
 
+/// Command-line interface definition.
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
@@ -25,31 +27,40 @@ struct Cli {
     mode: Mode,
 }
 
+/// Available printing modes.
 #[derive(Debug, Default, Clone, ValueEnum)]
 enum Mode {
+    /// Fill the screen immediately
     FillScreen,
 
+    /// Continuously print characters
     #[default]
     Infinite,
 
+    /// Randomly color individual cells over time
     Random,
 }
 
 impl fmt::Display for Mode {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        let str = match self {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let str = match *self {
             Self::FillScreen => "fill-screen",
             Self::Infinite => "infinite",
             Self::Random => "random",
         };
-        write!(fmt, "{str}")
+        write!(f, "{str}")
     }
 }
 
-fn fill_screen(cli: &Cli, mut writer: impl Write) -> io::Result<()> {
-    let (cols, rows) = size()?;
+/// Fills the terminal screen with `cli.char` in random colors,
+/// then blocks until the user quits.
+fn fill_screen<W>(cli: &Cli, mut writer: W) -> io::Result<()>
+where
+    W: Write,
+{
+    let (cols, rows) = terminal::size()?;
 
-    for _ in iproduct!(0..cols, 0..rows) {
+    for _ in itertools::iproduct!(0..cols, 0..rows) {
         queue!(
             writer,
             SetForegroundColor(generate_color()),
@@ -64,11 +75,19 @@ fn fill_screen(cli: &Cli, mut writer: impl Write) -> io::Result<()> {
     Ok(())
 }
 
+/// Generate a random ANSI Xterm system color.
+/// This is done to fully support Xterm and increase usability.
+///
+/// # Returns
+///
+/// A `Color::AnsiValue` in the range 0..16.
 #[must_use]
 fn generate_color() -> Color {
-    Color::AnsiValue(rand::random_range(0..8))
+    Color::AnsiValue(rand::random_range(0..16))
 }
 
+/// Waits the given duration for a keypress and returns a bool
+/// whether the Key quits the program.
 fn is_quitting_char_read(dur: Duration) -> io::Result<bool> {
     if event::poll(dur)? {
         if let Event::Key(
@@ -113,7 +132,11 @@ fn main() -> io::Result<()> {
     terminal::disable_raw_mode()
 }
 
-fn print_infinite(cli: &Cli, mut writer: impl Write) -> io::Result<()> {
+/// Continuously print `cli.char` at the current cursor position in random colors.
+fn print_infinite<W>(cli: &Cli, mut writer: W) -> io::Result<()>
+where
+    W: Write,
+{
     while !is_quitting_char_read(Duration::from_millis(10))? {
         execute!(
             writer,
@@ -125,16 +148,24 @@ fn print_infinite(cli: &Cli, mut writer: impl Write) -> io::Result<()> {
     Ok(())
 }
 
-fn print_random(cli: &Cli, mut writer: impl Write) -> io::Result<()> {
+/// Renders a grid of characters, changing the color of a single cell with each iteration.
+fn print_random<W>(cli: &Cli, mut writer: W) -> io::Result<()>
+where
+    W: Write,
+{
     let (columns, rows) = terminal::size()?;
-    let (columns, rows) = (usize::from(columns), usize::from(rows));
+    let (u_cols, u_rows) = (usize::from(columns), usize::from(rows));
 
-    let mut grid: Vec<Vec<Option<Color>>> = vec![vec![None; rows]; columns];
+    let mut grid: Vec<Vec<Option<Color>>> = vec![vec![None; u_rows]; u_cols];
 
+    #[expect(
+        clippy::indexing_slicing,
+        reason = "vec is guaranteed to be of correct length."
+    )]
     while !is_quitting_char_read(Duration::from_millis(10))? {
-        let (column, row) = (rand::random_range(0..columns), rand::random_range(0..rows));
+        let (rand_col, rand_row) = (rand::random_range(0..u_cols), rand::random_range(0..u_rows));
 
-        grid[column][row] = Some(generate_color());
+        grid[rand_col][rand_row] = Some(generate_color());
 
         for column in &grid {
             for row in column {
@@ -156,6 +187,7 @@ mod test {
     use crate::Cli;
     use clap::CommandFactory as _;
 
+    /// Verifies that the CLI flags/options do not conflict.
     #[test]
     fn verify_cli() {
         Cli::command().debug_assert();
