84f986f7b05a838608478313771ca907f129a7cf
KP64 <karamalsadeh@hotmail.com>
2025-08-15 14:47:27 +0200

    chore: remove unnecessary systems redefinition

--- nix/dev/flake-module.nix
+++ nix/dev/flake-module.nix
@@ -2,13 +2,6 @@
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
 
-  systems = [
-    "aarch64-darwin"
-    "aarch64-linux"
-    "x86_64-darwin"
-    "x86_64-linux"
-  ];
-
   perSystem =
     { self', pkgs, ... }:
     {
